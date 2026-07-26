aac28249e8880519c1ea871ea8a16fccd883f803
Pedro Carvalho <pedro8.carvalho@gmail.com>
2026-07-26 20:53:45 -0300

    Testa atualização de fatura existente e fatura inexistente

--- GerenciamentoFaturas.Test/Services/FaturaServiceTests.cs
+++ GerenciamentoFaturas.Test/Services/FaturaServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using GerenciamentoFaturas.Application.DTOs;
 using GerenciamentoFaturas.Application.Services;
 using GerenciamentoFaturas.Domain.Enums;
+using GerenciamentoFaturas.Domain.Exceptions;
 using GerenciamentoFaturas.Tests.Infrastructure;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -27,7 +28,7 @@ namespace GerenciamentoFaturas.Tests.Services
             var request = new FaturaRequestDto
             {
                 Numero = 1,
-                NomeCliente = "Pedro Carvalho",
+                NomeCliente = "Pedro",
                 DataEmissao = DateTime.Today
             };
             
@@ -67,6 +68,63 @@ namespace GerenciamentoFaturas.Tests.Services
             catch (ArgumentException ex)
             {
                 Assert.AreEqual("Nome do cliente obrigatório.", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void Atualizar_DeveAtualizarFaturaComSucesso()
+        {
+            var request = new FaturaRequestDto
+            {
+                Numero = 1,
+                NomeCliente = "Pedro",
+                DataEmissao = DateTime.Today
+            };
+
+            var criada = _service.Adicionar(request);
+
+            var atualizarRequest = new FaturaRequestDto
+            {
+                Numero = 2,
+                NomeCliente = "Maria",
+                DataEmissao = DateTime.Today.AddDays(1)
+            };
+
+            var response = _service.Atualizar(criada.Id, atualizarRequest);
+
+            Assert.IsNotNull(response);
+
+            Assert.AreEqual(2, response.Numero);
+            Assert.AreEqual("Maria", response.NomeCliente);
+            Assert.AreEqual(DateTime.Today.AddDays(1), response.DataEmissao);
+
+            var fatura = FaturaRepository.ObterPorId(criada.Id);
+
+            Assert.IsNotNull(fatura);
+            Assert.AreEqual(2, fatura.Numero);
+            Assert.AreEqual("Maria", fatura.NomeCliente);
+            Assert.AreEqual(DateTime.Today.AddDays(1), fatura.DataEmissao);
+        }
+
+        [TestMethod]
+        public void Atualizar_FaturaInexistente_DeveLancarFaturaNaoEncontradaException()
+        {
+            var request = new FaturaRequestDto
+            {
+                Numero = 1,
+                NomeCliente = "Pedro",
+                DataEmissao = DateTime.Today
+            };
+
+            try
+            {
+                _service.Atualizar(Guid.NewGuid(), request);
+
+                Assert.Fail("Era esperada uma FaturaNaoEncontradaException.");
+            }
+            catch (FaturaNaoEncontradaException ex)
+            {
+                Assert.AreEqual("Fatura não encontrada.", ex.Message);
             }
         }
     }
